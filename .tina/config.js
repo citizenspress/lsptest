e7304a55088afd7d8379bb4dc445b6da45081ede
Graham Cox <gcox@cpress.org>
2023-03-08 16:19:19 -0500

    npm now

--- .tina/config.js
+++ .tina/config.js
@@ -4,7 +4,7 @@ import { defineConfig } from "tinacms";
 const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 
 export default defineConfig({
-  branch: "main",
+  branch,
   clientId: "c6fbe233-0cd9-4bee-81c3-6e2892323445", // Get this from tina.io
   token: "b15bd9014d5bb530ffbb4f926af813fc5f1962f7", // Get this from tina.io
   build: {
@@ -20,82 +20,21 @@ export default defineConfig({
   schema: {
     collections: [
       {
-        label: "English",
-        name: "english",
-        path: "content/english",
+        name: "post",
+        label: "Posts",
+        path: "content/posts",
         fields: [
-          {
-            type: "rich-text",
-            name: "body",
-            label: "Body of Document",
-            description: "This is the markdown body",
-            isBody: true,
-          },
           {
             type: "string",
             name: "title",
-            label: "title",
-          },
-          {
-            type: "string",
-            name: "slug",
-            label: "slug",
+            label: "Title",
+            isTitle: true,
             required: true,
           },
           {
-            type: "string",
-            name: "description",
-            label: "description",
-          },
-          {
-            type: "string",
-            name: "summary",
-            label: "summary",
-            ui: {
-              component: "textarea",
-            },
-          },
-          {
-            type: "datetime",
-            name: "date",
-            label: "date",
-          },
-          {
-            type: "string",
-            name: "translationKey",
-            label: "translationKey",
-          },
-          {
-            type: "boolean",
-            name: "toc",
-            label: "table of contents",
-          },
-        ],
-      },
-      {
-        label: "French",
-        name: "french",
-        path: "content/french",
-        fields: [
-          {
             type: "rich-text",
             name: "body",
-            label: "Body of Document",
-            description: "This is the markdown body",
-            isBody: true,
-          },
-        ],
-      },
-      {
-        label: "Pages",
-        name: "pages",
-        path: "content",
-        fields: [
-          {
-            type: "rich-text",
-            name: "body",
-            label: "Body of Document",
-            description: "This is the markdown body",
+            label: "Body",
             isBody: true,
           },
         ],
